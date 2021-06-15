13b106aba9ee0fcb040f59687248da35de1b7f20
theopmw <theopmw@gmail.com>
2021-06-15 11:00:10 +0000

    Add delete_record function

--- mongo_project.py
+++ mongo_project.py
@@ -103,6 +103,30 @@ def edit_record():
             print("Error accessing the database")
 
 
+def delete_record():
+    doc = get_record()
+    if doc:
+        print("")
+        for k,v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + ": " + v.capitalize())
+
+        print("")
+        confirmation = input("Is this the document you want to delete?\nY or N >")
+        print("")
+
+        if confirmation. lower() == "y":
+            try:
+                coll.remove(doc)
+                print("Document deleted!")
+            except:
+                sprint("Error accessing the database")
+        else:
+            print("Document not deleted")
+            
+
+
+
 def main_loop():
     while True:
         option = show_menu()
@@ -113,7 +137,7 @@ def main_loop():
         elif option == "3":
             edit_record()
         elif option == "4":
-            print("You have selected option 4")
+            delete_record()
         elif option == "5":
             conn.close()
             break
